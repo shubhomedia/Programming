28f9a13c074bf10c32c2b89864db3cad1b9e32a6
shubhobd <shubhomedia@gmail.com>
2016-02-03 10:46:11 +0600

    double type data program added

---
+++ C/Simple Variable/double data type.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   double a,b,c,sum;
+   int x;
+
+   a = 10.10;
+   b = 10.20;
+   c = 10.30;
+   x = 10.00;
+   sum = a + b + c;
+
+   printf("a = %f,b = %f,c = %f,Sum of a + b + c = %f\n\n",a,b,c,sum);
+   printf("The Value of X is %d",x);
+   return 0;
+
+}
