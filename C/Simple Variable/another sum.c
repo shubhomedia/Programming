ed531793db916fb6fb54256855514b2fcf1fe108
shubhobd <shubhomedia@gmail.com>
2016-02-03 10:38:25 +0600

    two new program added 'simple sum and another sum'

---
+++ C/Simple Variable/another sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c,sum,cal;
+
+   a = 10;
+   b = 10;
+   c = 10;
+
+   sum = a+b+c;
+   cal = sum+c;
+
+   printf("1st Sum result is %d and 2nd is %d",sum,cal);
+
+   return 0;
+
+}
---
+++ C/Simple Variable/simple sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c,sum;
+
+   a = 10;
+   b = 10;
+   c = 10;
+
+   sum = a + b + c;
+
+   printf("a = %d,b = %d,c = %d,Sum of a + b + c = %d",a,b,c,sum);
+
+   return 0;
+
+}
