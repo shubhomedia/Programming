7f98447340ad95a407bb6a4df9cb0cdf88696951
shubhobd <shubhomedia@gmail.com>
2016-02-03 10:58:15 +0600

    sum of two number programm added with scanf function

---
+++ C/Simple Variable/scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter The Value of A\n");
+    scanf("%d",&a);
+    printf("Enter The Value of B\n");
+    scanf("%d",&b);
+
+    c = a+ b;
+    printf("Sum of A And B =%d",c);
+    return 0;
+}
