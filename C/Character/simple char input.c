15a0523137113f7c75658147bd7e802dd4923831
shubhobd <shubhomedia@gmail.com>
2016-02-12 16:14:52 +0600

    character type folder add and one simple program written

---
+++ C/Character/simple char input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char name;
+    printf("Enter Your Name: ");
+    scanf("%c", &name);
+    printf("The first letter of your name is: %c\n", name);
+
+    return 0;
+
+}
