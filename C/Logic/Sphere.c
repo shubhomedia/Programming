3494b46a4075ac568e2b95eeb0d1e9a7693c6fd7
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-29 23:26:38 +0600

    Sphere code added

---
+++ C/Logic/Sphere.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    double a,b,r,pi,result;
+    pi=3.14159;
+    scanf("%lf",&r);
+    result=(4.0/3*pi*r*r*r);
+    printf("VOLUME = %.3lf\n",result);
+
+    return 0;
+}
