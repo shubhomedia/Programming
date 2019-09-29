8a14358e3acbcf15adeed0170948ea4007fe3493
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-09-29 23:29:23 +0600

    Consumption of fuel

---
+++ C/Logic/Consumption.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b,result;
+    scanf("%d",&a);
+    scanf("%f",&b);
+    result=(a/b);
+    printf("%.3lf km/l\n",result);
+
+    return 0;
+}
