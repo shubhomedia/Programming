63f2af3e9c1b639a4c985cccab3cfc3cc39003a4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-07 22:13:04 +0600

    new code stack added

---
+++ C/LinkList/Stack/simple_stack_print_data.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+
+#define CAPACITY 10000
+
+struct stack
+{
+    int data;
+    struct stack *next;
+}*top;
+
+int size = 0;
+
+int main()
+{
+    int element;
+    printf("Enter your data:");
+    scanf("%d",&element);
+    push(element);
+    display();
+    printf("Enter your data:");
+    scanf("%d",&element);
+    push(element);
+    display();
+
+
+
+}
+
+void push(int element)
+{
+    if (size >= CAPACITY)
+    {
+        printf("Stack Overflow, No Space to add more element in stack.\n");
+        return ;
+    }
+    struct stack * newNode = (struct stack *) malloc(sizeof(struct stack));
+
+    newNode->data = element;
+    newNode->next = top;
+
+    top = newNode;
+    size++;
+    printf("Data pushed to stack.\n");
+};
+
+void display()
+{
+    struct stack* temp;
+
+    if (top == NULL) {
+        printf("Stack Underflow");
+        exit(1);
+    }
+    else {
+        temp = top;
+        while (temp != NULL) {
+            printf("Data :%d\n",temp->data);
+            temp = temp->next;
+        }
+    }
+};
