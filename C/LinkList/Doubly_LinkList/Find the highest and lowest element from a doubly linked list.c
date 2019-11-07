fa88764d0c9db951236a1ffed71bd91fdbc853e4
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-07 21:20:15 +0600

    Find the highest and lowest element from a doubly linked list

---
+++ C/LinkList/Doubly_LinkList/Find the highest and lowest element from a doubly linked list.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+
+struct node {
+    int data;
+    struct node *next;
+    struct node *prev;
+}*head;
+
+void create_linklist();
+void displaydata();
+int largestElement();
+int smallestElement();
+
+void main(){
+    int n;
+    printf("Enter Total Number of node: ");
+    scanf("%d",&n);
+    create_linklist(n);
+    displaydata();
+    largestElement(head);
+    smallestElement(head);
+}
+
+void create_linklist(int n){
+    struct node *p,*temp;
+    int element;
+    printf("Enter your first element for Node 1: ");
+    scanf("%d",&element);
+    head = (struct node *)malloc(sizeof(struct node));
+    head->prev = NULL;
+    head->data = element;
+    head->next = NULL;
+    temp = head;
+    for(int i = 2 ;i<=n;i++){
+        printf("Enter your first element for Node %d: ",i);
+        scanf("%d",&element);
+        p = (struct node *)malloc(sizeof(struct node));
+        p->prev = head;
+        p->data = element;
+        p->next = NULL;
+        temp->next=p;
+        temp=temp->next;
+        };
+};
+
+void displaydata(){
+    struct node *temp;
+        temp=head;
+        while(temp!=NULL){
+            printf("Data = %d\n",temp->data);
+            temp = temp->next;
+        }
+    }
+
+int largestElement(struct node * head)
+{
+    int max = INT_MIN;
+    while(head!=NULL){
+        if(max<head->data)
+            max=head->data;
+        head=head->next;
+    }
+    printf("Highest  Value is: %d\n",max);
+}
+
+int smallestElement(struct node * head)
+{
+    int min = INT_MAX;
+    while(head!=NULL){
+        if(min>head->data)
+            min=head->data;
+        head=head->next;
+    }
+    printf("Lowest Value is: %d\n",min);
+}
+
+
