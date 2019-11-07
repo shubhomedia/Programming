38651bc753a26223f78b22f4b8594125e9d95fdb
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-07 20:48:34 +0600

    new doubly linklist code added

---
+++ C/LinkList/Doubly_LinkList/n node doubly_linklist with print.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+    struct node *prev;
+}*head;
+
+int main(){
+    int node_number;
+    int elements;
+    printf("How many Node? \n");
+    scanf("%d",&node_number);
+    create_linklist(node_number);
+    show_linklist();
+
+};
+
+void create_linklist(int n){
+    struct node *p,*temp;
+    int element;
+    printf("Enter your first element for Node 1: ");
+    scanf("%d",&element);
+    head = (struct node *)malloc(sizeof(struct node));
+    head->prev = NULL;
+    head->data = element;
+    head->next = NULL;
+    temp = head;
+    for(int i = 2 ;i<=n;i++){
+        printf("Enter your first element for Node %d: ",i);
+        scanf("%d",&element);
+        p = (struct node *)malloc(sizeof(struct node));
+        p->prev = head;
+        p->data = element;
+        p->next = NULL;
+        temp->next=p;
+        temp=temp->next;
+        };
+};
+
+void show_linklist(){
+    struct node *temp;
+    temp=head;
+    int counter = 1;
+    while(temp!=NULL){
+        printf("Data %d = %d\n",counter,temp->data);
+        temp= temp->next;
+        counter++;
+    }
+};
