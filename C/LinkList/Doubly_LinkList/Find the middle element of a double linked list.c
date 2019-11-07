6ac3bf9cc3ee5c3824a4812df369d28748b20a8b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2019-11-07 21:31:31 +0600

    Find the middle element of a double linked list

---
+++ C/LinkList/Doubly_LinkList/Find the middle element of a double linked list.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+
+struct node {
+    int data;
+    struct node *next;
+    struct node *prev;
+}*head;
+
+void create_linklist();
+void displaydata();
+void printMiddle();
+
+void main(){
+    int n;
+    printf("Enter Total Number of node: ");
+    scanf("%d",&n);
+    create_linklist(n);
+    displaydata();
+    printMiddle(head);
+}
+
+void create_linklist(int n){
+    struct node *p,*temp;
+    int element;
+    printf("Enter your first element for Node 1: ");
+    scanf("%d",&element);
+    head = (struct node *)malloc(sizeof(struct node));
+    head->prev = NULL;
+    head->data = element;
+    head->next = NULL;
+    temp = head;
+    for(int i = 2 ;i<=n;i++){
+        printf("Enter your first element for Node %d: ",i);
+        scanf("%d",&element);
+        p = (struct node *)malloc(sizeof(struct node));
+        p->prev = head;
+        p->data = element;
+        p->next = NULL;
+        temp->next=p;
+        temp=temp->next;
+        };
+};
+void displaydata(){
+    struct node *temp;
+        temp=head;
+        while(temp!=NULL){
+            printf("Data = %d\n",temp->data);
+            temp = temp->next;
+        }
+    }
+
+void printMiddle(struct node *head)
+{
+    struct node *slow_ptr = head;
+    struct node *fast_ptr = head;
+
+    if (head!=NULL)
+    {
+        while (fast_ptr != NULL && fast_ptr->next != NULL)
+        {
+            fast_ptr = fast_ptr->next->next;
+            slow_ptr = slow_ptr->next;
+        }
+        printf("The middle element is %d\n", slow_ptr->data);
+    }
+}
+
+
